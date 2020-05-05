cc0260e9f68a96e431e5de2922b7a61c8d0ef837
brandon quezada <brandon@sardius.media>
2020-05-05 15:22:45 -0500

    getting timeleft component working

--- src/components/TimeLeft.jsx
+++ src/components/TimeLeft.jsx
@@ -1,13 +1,35 @@
 import moment from "moment";
 import momentDurationFormatSetup from "moment-duration-format";
 import React, { useState } from "react";
+import { useEffect } from "react";
 
 momentDurationFormatSetup(moment);
 
 const TimeLeft = ({ SessionLength }) => {
   const [timeLeft, setTimeLeft] = useState(SessionLength);
+
+  // change timeLeft whenever sessionLength changes
+  useEffect(() => {
+    setTimeLeft(SessionLength);
+  }, [SessionLength]);
+
+  const handleStartStopClick = () => {
+    //    decrement timeLeft by one every second (1000 ms)
+    //      to do this we'll use setInterval
+
+    setInterval(() => {
+      console.log("hello there!");
+    }, 1000);
+  };
+
   const formattedTimeLeft = moment.duration(timeLeft, "s").format("mm.ss");
-  return <div>{formattedTimeLeft}</div>;
+
+  return (
+    <div>
+      {formattedTimeLeft}
+      <button onClick={handleStartStopClick}>Start</button>
+    </div>
+  );
 };
 
 export default TimeLeft;
